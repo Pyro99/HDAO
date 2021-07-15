d0fd3be08ccf175ae4f111b199f11cafe649c047
user <user@DESKTOP-RVVQMMC>
2021-07-15 16:51:37 +0530

    read values from forms

--- BookFrontendDemo/src/main/java/com/niit/UserController.java
+++ BookFrontendDemo/src/main/java/com/niit/UserController.java
@@ -1,21 +1,38 @@
 package com.niit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.niit.dao.UserDAO;
+import com.niit.model.User;
 //
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	UserDAO userDAO;
+	
 	@RequestMapping("/login")
-	public String m1()
+	public String login()
 	{
 		return "login";
 	}
 	
 	@RequestMapping("/register")
-	public String m2()
+	public String register(ModelMap map)
 	{
+		map.addAttribute("user",new User());
 		return "register";
+	}
+	
+	@RequestMapping("/add")
+	public String add(@ModelAttribute("user") User u)
+	{
+		userDAO.addUser(u);
+		return "redirect:/login";
 	}
 }
